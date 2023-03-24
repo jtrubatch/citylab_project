aa7ce1b5d9df31022a03c78a99f6c3e5eae25d06
Joel Trubatch <jtrubatch@gmail.com>
2023-03-24 15:11:57 +0000

    Updated patrol behavior, ready for robotlab testing through task 2

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -5,10 +5,9 @@
 #include <rclcpp/rclcpp.hpp>
 #include <geometry_msgs/msg/twist.hpp>
 #include <sensor_msgs/msg/laser_scan.hpp>
-#include <nav_msgs/msg/odometry.hpp>
 #include <vector>
 #include <chrono>
-#include <iterator>
+
 
 using std::placeholders::_1;
 using namespace std::chrono_literals;
@@ -44,84 +43,96 @@ public:
 private:
   void sensorCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg)  // 0/720 Rear  180 Right 360 Front 540 Left
 	{
-		
-		this->front = msg->ranges[360];
-		this->laser = {msg->ranges[180], msg->ranges[225], msg->ranges[270], msg->ranges[315], msg->ranges[360],
-									 msg->ranges[405], msg->ranges[450], msg->ranges[495], msg->ranges[540]};
+    // Data from 180 to 540 increments of 20.
+		std::vector<float> right_side = {
+            msg->ranges[180], msg->ranges[200], msg->ranges[220], 
+            msg->ranges[240], msg->ranges[260], msg->ranges[280]  
+    };    
+		std::vector<float> front = { // 9 is Center/360
+            msg->ranges[300], msg->ranges[320], msg->ranges[340], 
+            msg->ranges[360], msg->ranges[380], msg->ranges[400], msg->ranges[420]
+    };
+    std::vector<float> left_side = {
+            msg->ranges[440], msg->ranges[460], msg->ranges[480], 
+            msg->ranges[500], msg->ranges[520], msg->ranges[540]
+    };
 
-		auto min_iterator = std::min_element(laser.begin(), laser.end());
-		this->laser_min_index = std::distance(laser.begin(), min_iterator);
-		this->laser_min = laser[laser_min_index];
+		left_min = *min_element(left_side.begin(),left_side.end());
+		left_max = *max_element(left_side.begin(),left_side.end());
+		right_min = *min_element(right_side.begin(),right_side.end());
+		right_max = *max_element(right_side.begin(),right_side.end());
+		front_min = *min_element(front.begin(), front.end());
+    
 	}
+
   void commandPublisher()  // positive CCW negative CW
 	{	
+    float linear_vel = 0.15;
+
+    if(left_min < side_min && front_min > min_distance)
+		{
+			cmd.linear.x = linear_vel * 0.5;
+		  cmd.angular.z = -0.15;
+		}else if(right_min < side_min && front_min > min_distance)
+		{
+			cmd.linear.x = linear_vel * 0.5;
+		  cmd.angular.z = 0.15;
+		}else if(front_min > min_distance)
+		{
+			cmd.linear.x = linear_vel;
+		  cmd.angular.z = 0.0;
+
+		}else if(front_min < min_distance) 
+		{
+			if(compareSidesLeft(left_max, right_max))
+			{
+				cmd.linear.x = linear_vel * 0.25;
+		    cmd.angular.z = 0.35;
+
+			}
+			else if(!compareSidesLeft(left_max, right_max))
+			{
+				cmd.linear.x = linear_vel * 0.25;
+		    cmd.angular.z = -0.35;
+
+			}		
+    } 
+
 		this->cmd_pub->publish(cmd);
+	}
 
-		cmd.linear.x = linearControl(front);
-		cmd.angular.z = angularControl(laser_min, laser_min_index );
-
-		this->cmd_pub->publish(cmd);
-
+  bool compareSidesLeft(float const &left, float const &right)
+	{
+		if(left >= right)
+		{
+			return true;
+		}
+		else 
+		{
+			return false;
+		}	
 	}
 
 	void infoMsg()
 	{
 		RCLCPP_INFO(this->get_logger(), "Linear Velocity: %f   Angular Velocity %f", cmd.linear.x, cmd.angular.z);
-		RCLCPP_INFO(this->get_logger(), "Front Laser: %f   Min Index: %d  Min Value: %f", front, laser_min_index, laser_min);
 	}
 
-	float linearControl(float distance) // 0 to 0.75
-	{
-		float clamped_front;
-		if(distance > 0.75)
-		{
-			clamped_front = 0.75;
-		}
-		else if(distance < min_distance)
-		{
-			clamped_front = min_distance;
-		}
-		else 
-		{
-			clamped_front = distance;
-		}
-
-		return (clamped_front - min_distance) * 0.25;
-	}
-
-	float angularControl(float range, int index)
-	{
-		float vel = 0.0;
-		if(range <= obstacle_buffer)
-		{
-			if(index < 4 ) // Object on right, turn left
-			{
-				vel = 0.2;
-			}
-			else if(index >= 4) //Object on left or straight ahead, turn right
-			{
-				vel = -0.2;
-			}
-
-		}
-		return vel;
-	}
+	
 	rclcpp::CallbackGroup::SharedPtr patrol_group;
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_sub;
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_pub;
 	rclcpp::TimerBase::SharedPtr control_timer;
 	rclcpp::TimerBase::SharedPtr msg_timer;
-  
-	float front;
-	std::vector <float> laser;
-	float laser_min;
-	int laser_min_index;
 	geometry_msgs::msg::Twist cmd;
 	
-	
-	float obstacle_buffer = 0.35;
-	float min_distance = 0.25;
-	float max_buffer = 0.1;
+	float left_min = 0.0;
+	float left_max = 0.0;
+  float front_min = 0.0;
+  float right_min = 0.0;
+	float right_max = 0.0;
+	const float min_distance = 0.5;
+	const float side_min = 0.25;
 };
 
 int main(int argc, char **argv)
